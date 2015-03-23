6d325d68c1efc901c23eb92d8d08715feaa34f47
Miguel Angel Monrroy <angel.xydj@gmail.com>
2015-03-23 01:21:06 -0600

    solo4
    faltan 4

--- PracticaUno_201020782/src/NivelDos.java
+++ PracticaUno_201020782/src/NivelDos.java
@@ -1,43 +1,254 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 
 public class NivelDos {
 	private Scanner scanDos = new Scanner(System.in);
 
+	public void limpiarPantalla(){
+		for (int i = 0; i < 40; i++) {
+			System.out.println("");
+		}
+	}
+	
+	public void pausa(){
+		System.out.println("\nPresione [Enter] para continuar...");
+		try {
+			System.in.read();
+			for (int i = 0; i < 40; i++) {
+				System.out.println("");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void problema6(){
-
+		int valor1 = 0, valor2 = 0, bandera = 0;
+		String operador = "";
+		limpiarPantalla();
+		System.out.println("[Problema 6 - Nivel 2]");
+		System.out.println("-----------------------------------");
+		System.out.printf("%10s%10s\n","Opcion", "Detalles");
+		System.out.println("-----------------------------------");
+		System.out.printf("%8s%26s\n","s", "Suma entre dos numeros.");
+		System.out.printf("%8s%27s\n","r", "Resta entre dos numeros.");
+		System.out.printf("%8s%36s\n","m", "Multiplicacion entre dos numeros.");
+		System.out.printf("%8s%30s\n","d", "Cociente entre dos numeros.");
+		
+			do{
+				System.out.println("Ingrese opción:");
+				operador = scanDos.next();
+				switch (operador.charAt(0)){
+					case 's':
+					case 'r':
+					case 'm':
+					case 'd':
+						bandera = 1;
+						break;
+				    default:
+				    	bandera = 0;
+						break;
+				}
+			}while((operador.length()>1) || bandera==0 );
+			
+			System.out.println("Ingrese el primer número: ");
+			valor1 = scanDos.nextInt();
+			System.out.println("Ingrese el segundo número: ");
+			valor2 = scanDos.nextInt();
+			
+			switch (operador.charAt(0)){
+				case 's':
+					System.out.println("La suma de los numeros es: " + valor1 + "+" + valor2 + " = " + (valor1+valor2));
+					break;
+				case 'r':
+					System.out.println("La resta de los numeros es: " + valor1 + "-" + valor2 + " = " + (valor1-valor2));
+					break;
+				case 'm':
+					System.out.println("La multiplicación de los numeros es: " + valor1 + "*" + valor2 + " = " + (valor1*valor2));
+					break;
+				case 'd':
+					System.out.println("El cociente de los numeros es: " + valor1 + "/" + valor2 + " = " + (valor1/valor2));
+					break;
+				default:
+					System.out.println("error");
+					break;
+			}
+	
+		pausa();
 	}
 
 	public void problema7(){
-
+		limpiarPantalla();
+		int num1;
+		System.out.println("[Problema 7 - Nivel 2]");
+		System.out.println("Mostrar la tabla de multiplicar en orden descendente\nde número entre 1 y 10.");
+		do{
+			System.out.println("Ingrese un número:");
+			num1 = scanDos.nextInt();
+			if(num1<1 || num1>10){
+				System.out.println("*valores entre 1 y 10.");
+			}
+		}while(num1<1 || num1>10);
+		
+		System.out.println("TABLA DEL NUMERO " + num1);
+		for (int i = 10; i > 0; i--) {
+			System.out.printf("%4s%4s%4s%4s%4s\n", num1 ,"*",i,"=",(num1*i));
+		}
+		pausa();
 	}
 
 	public void problema8(){
+		limpiarPantalla();
+		System.out.println("[Problema 8 - Nivel 2]");
 
 	}
 
 	public void problema9(){
+		limpiarPantalla();
+		System.out.println("[Problema 9 - Nivel 2]");
 
 	}
 	public void problema10(){
+		limpiarPantalla();
+		int num1 = 0, num2=1 ,espacios=0;
+		String linea = "";
+		
+		System.out.println("[Problema 10 - Nivel 2]");
+		System.out.println("Piramide invertida de números");
+		System.out.println("Ingrese un número entero:");
+		num1 = scanDos.nextInt();
+		espacios = num1*2;
+		do{
+			for (int i = (num1+1); i >1; i--) {
+				linea+=num2+" ";
+			}
+			System.out.printf("%"+espacios+"s\n",linea);
+			num1--;
+			espacios--;
+			num2++;
+			linea="";
+		}while(num1>0);
+		pausa();
+		
 
 	}
 
 	public void problema11(){
+		limpiarPantalla();
+		System.out.println("[Problema 11 - Nivel 2]");
 
 	}
 	public void problema12(){
-
+		limpiarPantalla();
+		String cadena= "", linea = "";
+		
+		System.out.println("[Problema 12 - Nivel 2]");
+		System.out.println("Cambiar letras por asteriscos.");
+		System.out.println("Ingrese una cadena de texto:");
+		cadena = scanDos.nextLine();
+		
+		for (int i = 0; i < cadena.length(); i++) {
+			
+			if(cadena.charAt(i)!=' '){
+				linea+="*";
+			}else{
+				linea+=" ";
+			}
+		}
+		
+		System.out.println("Cadena de texto ingresada:   "+cadena);
+		System.out.println("Cadena de texto modificada:  "+linea);
+		pausa();
 	}
 
 	public void problema13(){
-
+		limpiarPantalla();
+		String letra = "", intento="";
+		
+		System.out.println("[Problema 13 - Nivel 2]");
+		System.out.println("Encuentra la letra.");
+		do{
+		System.out.println("Ingresa una letra [A-Z]:");
+		letra = scanDos.nextLine();
+		letra = letra.toUpperCase();
+		
+			if(letra.length()!=1){
+				System.out.println("Error en el ingreso.");
+				letra="4";
+			}else if(letra.codePointAt(0)<65 || letra.codePointAt(0)>90){
+				System.out.println("Error en el ingreso de la letra.\nSolo una letra.");
+			}else{
+				limpiarPantalla();
+				System.out.println("Letra guardada exitosamente...\nQue empiece el juego... ");
+			}
+		}while(letra.codePointAt(0)<65 || letra.codePointAt(0)>90); 
+		
+		do{
+			System.out.println("Ingresa una letra:");
+			intento = scanDos.nextLine();
+			
+			if(intento.length()==1){
+				intento = intento.toUpperCase();
+				if(intento.codePointAt(0)>64 && intento.codePointAt(0)<91){
+					if(intento.codePointAt(0)==letra.codePointAt(0)){
+						System.out.println("Exito, Encontraste la letra. La letra buscada era la: " +letra);
+					}else if(intento.codePointAt(0)>letra.codePointAt(0)){
+						System.out.println("Atras...");
+					}else {
+						System.out.println("Adelante...");
+					}
+					
+				}else{
+					System.out.println("Error en el ingreso.\nSolo letras.");
+				}
+			}else{
+				System.out.println("Error en el ingreso.");
+			}
+			
+			
+		}while(!intento.equals(letra));
+		
+		pausa();
 	}
+	
 	public void problema14(){
+		limpiarPantalla();
+		String cadh="", cadv="*";
+		System.out.println("[Problema 14 - Nivel 2]");
+		System.out.println("Dibujar un cuadrado de 'n' elementos:");
+		System.out.println("Ingrese un número entero:");
+		int num = scanDos.nextInt();
+		
+		if(num == 0){
+			System.out.println("No hay cuadrado... =0)");
+		}else if(num == 1){
+			System.out.println("*");
+		}else if(num==2){
+			System.out.println("**\n**");
+		}else{
+			for (int i = 0; i < num; i++) {
+				cadh+="*";
+			}
+			System.out.println(cadh);
+			
+			for (int i = 0; i < (num-2); i++) {
+				System.out.printf("%s%"+(num-1)+"s\n", "*","*");
+			}
+			System.out.println(cadh);
+		}
+		
+		pausa();
 
 	}
 
 	public void problema15(){
-
+		limpiarPantalla();
+		DecimalFormat dcmls = new DecimalFormat("#.##");
+		double num = 0, contador =0, conta2=0;
+		pausa();
 	}
+	
 }
--- PracticaUno_201020782/src/Principal.java
+++ PracticaUno_201020782/src/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 	private NivelUno nUno = new NivelUno();
 	private NivelDos nDos = new NivelDos();
 	private int opcApp1 = 0, opcApp2 = 0; 
-	private int opcUno = 0, opcDos = 0;
+	private int opcUno = 0;
 	
 	public static void main(String[] args) {
 		Principal pl = new Principal();
@@ -85,20 +85,70 @@ public class Principal {
 	}
 	
 	public void menuAppDos(){
-		limpiarPantalla();
-		System.out.println("********** APLICACIONES NIVEL 2 **********");
-		System.out.printf("%8s%13s%15s", "Opción","Actividad", "Detalles\n");
-		System.out.printf("%5s%17s%29s","6", "Problema 6","Figura Rombo Asteriscos\n");
-		System.out.printf("%5s%17s%26s","7", "Problema 7","Notas del estuciante\n");
-		System.out.printf("%5s%17s%18s","8", "Problema 8","Validar Hora\n");
-		System.out.printf("%5s%17s%25s","9", "Problema 9","Figuras geometricas\n");
-		System.out.printf("%6s%17s%23s","10", "Problema 10","Dias entre fechas\n");
-		System.out.printf("%6s%17s%26s","11", "Problema 11","Regruesar Anterior\n");
-		System.out.printf("%6s%17s%26s","12", "Problema 12","Notas del estuciante\n");
-		System.out.printf("%6s%17s%18s","13", "Problema 13","Validar Hora\n");
-		System.out.printf("%6s%17s%25s","14", "Problema 14","Figuras geometricas\n");
-		System.out.printf("%6s%17s%23s","15", "Problema 15","Dias entre fechas\n");
-		System.out.printf("%5s%15s%26s","0", "Reguesar","Regruesar Anterior\n");
+		do{
+			limpiarPantalla();
+			System.out.println("********** APLICACIONES NIVEL 2 **********");
+			System.out.println("----------------------------------------");
+			System.out.printf("%8s%13s%15s", "Opción","Actividad", "Detalles\n");
+			System.out.println("----------------------------------------");
+			System.out.printf("%5s%17s%24s","6", "Problema 6","Calculadora simple.\n");
+			System.out.printf("%5s%17s%26s","7", "Problema 7","Tabla de multiplicar.\n");
+			System.out.printf("%5s%17s%25s","8", "Problema 8","Aleatorio en letras.\n");
+			System.out.printf("%5s%17s%22s","9", "Problema 9","Arabigo a romano.\n");
+			System.out.printf("%6s%17s%23s","10", "Problema 10","Piramide invertida.\n");
+			System.out.printf("%6s%17s%20s","11", "Problema 11","Numeros romanos.\n");
+			System.out.printf("%6s%17s%22s","12", "Problema 12","Cadena asteriscos.\n");
+			System.out.printf("%6s%17s%20s","13", "Problema 13","Juego de letras.\n");
+			System.out.printf("%6s%17s%27s","14", "Problema 14","Cuadrado de asteriscos.\n");
+			System.out.printf("%6s%17s%19s","15", "Problema 15","Números primos.\n");
+			System.out.printf("%5s%15s%25s","5", "Reguesar","Regruesar Anterior\n");
+			do{
+				System.out.println("Ingrese opción:");
+				opcApp2 = scan.nextInt();
+				switch (opcApp2){
+				case 5:limpiarPantalla();
+					
+					break;
+				case 6:limpiarPantalla();
+						nDos.problema6();
+					break;
+				case 7:limpiarPantalla();
+						nDos.problema7();
+					break;
+				case 8:limpiarPantalla();
+						nDos.problema8();
+					break;
+				case 9:limpiarPantalla();
+						nDos.problema9();
+					break;
+				case 10:limpiarPantalla();
+						nDos.problema10();
+					break;
+				case 11:limpiarPantalla();
+						nDos.problema11();
+					break;
+				case 12:limpiarPantalla();
+						nDos.problema12();
+					break;
+				case 13:limpiarPantalla();
+						nDos.problema13();
+					break;
+				case 14:limpiarPantalla();
+						nDos.problema14();
+					break;
+				case 15:limpiarPantalla();
+						nDos.problema15();
+					break;
+				default:
+					break;
+				}
+			}while(opcApp2<5 || opcApp2>15);
+			
+			if(opcApp2 == 5){
+				opcApp2 = 0;
+			}
+			
+		}while(opcApp2!=0);
 	}
 	
 	public void limpiarPantalla(){
